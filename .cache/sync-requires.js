6cb9bcc0fef77180ae167c175e3a1eeb814ed222
Devarajan Nachimuthu <ndevaraj424@gmail.com>
2021-07-24 02:35:40 +0530

    Added one more project

---
+++ .cache/sync-requires.js
@@ -0,0 +1,10 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("D:\\Life\\itsDeva\\ItsDeva\\.cache\\dev-404-page.js")))
+}
+
